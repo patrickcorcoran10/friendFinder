c19f4c02dedfa24ca8dc5cea77333e390a2f1f37
Patty C <patrickcorcoran10@gmail.com>
2018-08-25 18:14:35 -0500

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # friendFinder
+
+### Overview
+
+This friendFinder app is a full-stacck site that will use Express to handle routing betweeen the survey page, the home page, and the api page. 
+
+It will utilize Heroku to deploy.
+
+## Issues for the TA
+
+I was not able to get the fundamental logic to work. I tried to google some examples of it working and wasn't able to fully implement what I learned. 
+
+I had issues parseInt-ing the newFriend scores so that they would be helpful in the math.
+
+Printing to the survey page was also unable to be completed. I was able to collect a name and a photo but not able to get the modal to successfully work.
+
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -23,34 +23,37 @@ app.post("/api/friends", function(req, res) {
     var newFriend = req.body;
     var newFriendScore = parseInt(newFriend.scores);
     
+    
     // Create Variables for Printing Best Match
     var bestMatchName = "";
     var bestMatchPhoto = "";
     var topScore = 100;
-
+    // Run a set of for loops to pull the friendsData's scores as well as the newFriend's.
     for (var i = 0; i < friendsData.length; i++) {
         var idealScore = 0;
-        for (var x = 0; x < newFriend.length; x++) {
+        for (var x = 0; x < newFriendScore.length; x++) {
             idealScore += Math.abs(friendsData[i].scores[x] - newFriendScore[x])
         }
+        // Test the Scores ofs of the newFriend against a hypothetical "topScore" to find the max difference. 
         if (idealScore < topScore) {
             bestMatchName = friendsData[i].name;
             bestMatchPhoto = friendsData[i].photo;
         }
         
     };
-    console.log(bestMatchName);
+    console.log(newFriendScore);
         
     friendsData.push(newFriend);
     res.json(newFriend);
-    
-    
+    // Print the 
+    console.log(bestMatchName);
+    console.log(bestMatchPhoto);
+    res.json({ status: "OK", matchName: bestMatchName, matchPhoto: bestMatchPhoto})
 });
 
 
-app.post("/api/clear", function() {
-    // friendData = [];
 
-    // console.log(friendsData);
+app.post("/api/clear", function() {
+    
 });
 };
